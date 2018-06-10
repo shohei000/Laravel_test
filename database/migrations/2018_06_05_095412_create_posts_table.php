8084d78fc48c6daf81a1003cf290a44aa086b466
dyoshikawa <dyoshikawa1993@gmail.com>
2018-06-10 23:55:14 +0900

    PostCommentSeeder動作するよう修正

--- database/migrations/2018_06_05_095412_create_posts_table.php
+++ database/migrations/2018_06_05_095412_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('cat_id'); // ポストテーブルとカテゴリーテーブルの紐付けに利用します
             $table->text('content');
-            $table->unsignedInteger('comment_count'); // 投稿に何件のコメントがついたのかをカウントします
+            $table->unsignedInteger('comment_count')->default(0); // 投稿に何件のコメントがついたのかをカウントします
             $table->timestamps();
         });
     }
