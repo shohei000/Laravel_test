739c69d5b354fa9f4edb0f0ebef3b935c3470456
saino <sainoshohei@dande-lion.co.jp>
2018-06-10 22:11:10 +0900

    first

---
+++ app/models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Post extends Model{
+  public function Comments(){
+    // 投稿はたくさんのコメントを持つ
+    return $this->hasMany('Comment', 'post_id');
+  }
+
+  public function Category(){
+    // 投稿は1つのカテゴリーに属する
+    return $this->belongsTo('Category','cat_id');
+  }
+
+}
